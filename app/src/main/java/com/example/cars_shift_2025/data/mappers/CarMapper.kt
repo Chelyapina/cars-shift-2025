fbb74ddfe9ccd48f4d4a771d6a1836ff973112d9
Chelyapina <katechelya@gmail.com>
2025-07-03 00:16:49 +0700

    feat(data/mappers): add CarMapper

---
+++ app/src/main/java/com/example/cars_shift_2025/data/mappers/CarMapper.kt
@@ -0,0 +1,46 @@
+package com.example.cars_shift_2025.data.mappers
+
+import com.example.cars_shift_2025.data.models.OneCarDto
+import com.example.cars_shift_2025.domain.models.CarWithRents
+import com.example.cars_shift_2025.domain.models.Car
+import com.example.cars_shift_2025.domain.models.Steering
+
+class CarMapper {
+
+    fun toCar(oneCarDto : OneCarDto): Car {
+        return Car(
+            id = oneCarDto.id,
+            name = oneCarDto.name,
+            brand = oneCarDto.brand.toDomain(),
+            imageUrl = BASE_IMAGE_URL + oneCarDto.media.first(),
+            transmission = oneCarDto.transmission.toDomain(),
+            price = oneCarDto.price
+        )
+    }
+
+    fun toCarWithRents(oneCarDto : OneCarDto): CarWithRents {
+        return CarWithRents(
+            id = oneCarDto.id,
+            name = oneCarDto.name,
+            brand = oneCarDto.brand.toDomain(),
+            imageUrl = BASE_IMAGE_URL + oneCarDto.media.first(),
+            transmission = oneCarDto.transmission.toDomain(),
+            price = oneCarDto.price,
+            color = oneCarDto.color.toDomain(),
+            bodyType = oneCarDto.bodyType.toDomain(),
+            steering = oneCarDto.steering?.toDomain() ?: DEFAULT_STEERING_VALUE,
+            startDate = oneCarDto.rents?.first() ?: DEFAULT_DATE_VALUE ,
+            endDate = oneCarDto.rents?.last() ?: DEFAULT_DATE_VALUE
+        )
+    }
+
+    fun toListCar(list : List<OneCarDto>): List<Car> {
+        return list.map { toCar(it) }
+    }
+
+    companion object{
+        private const val BASE_IMAGE_URL = "https://shift-intensive.ru/"
+        private val DEFAULT_STEERING_VALUE : Steering = Steering.LEFT
+        private const val DEFAULT_DATE_VALUE = 0L
+    }
+}
