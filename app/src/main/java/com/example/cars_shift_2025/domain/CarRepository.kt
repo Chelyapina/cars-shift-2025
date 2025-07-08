4d6cfdc2779787cb4e4015e272fa93884f627861
Chelyapina <katechelya@gmail.com>
2025-07-08 16:54:43 +0700

    optimize(data/CarRepositoryImpl & domain/usecases & domain/CarRepository) optimize logic of error handling - Добавление класса Result для обработки исключений - Внедрение паттерна проксирование (сори что не в прошлом pr) - Убраны логи

--- app/src/main/java/com/example/cars_shift_2025/domain/CarRepository.kt
+++ app/src/main/java/com/example/cars_shift_2025/domain/CarRepository.kt
@@ -1,9 +1,9 @@
 package com.example.cars_shift_2025.domain
 
-import com.example.cars_shift_2025.domain.models.CarWithRents
 import com.example.cars_shift_2025.domain.models.Car
+import com.example.cars_shift_2025.domain.models.CarWithRents
 
 interface CarRepository {
-    suspend fun getCarsList(): List<Car>
-    suspend fun getCarById(id: String): CarWithRents
+    suspend fun getCarsList() : Result<List<Car>>
+    suspend fun getCarById(id : String) : Result<CarWithRents>
 }
