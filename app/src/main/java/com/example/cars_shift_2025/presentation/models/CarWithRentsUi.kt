1f4b22a280545ee4a452cce6d7ae0c7c19441fb2
Chelyapina <katechelya@gmail.com>
2025-07-08 17:09:31 +0700

    feat(presentation/models): add ui models

---
+++ app/src/main/java/com/example/cars_shift_2025/presentation/models/CarWithRentsUi.kt
@@ -0,0 +1,13 @@
+package com.example.cars_shift_2025.presentation.models
+
+data class CarWithRentsUi(
+    val imageRes: String,
+    val title: String,
+    val transmission: String,
+    val steering: String,
+    val bodyType: String,
+    val color: String,
+    val price: String,
+    val startDate: Long?,
+    val endDate: Long?
+)
