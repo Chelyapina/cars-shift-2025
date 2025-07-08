0f465bfcb35813ebfdd8b175ccdf35e7b9696404
Chelyapina <katechelya@gmail.com>
2025-07-08 17:17:07 +0700

    feat(presentation/navigation): add navigation

---
+++ app/src/main/java/com/example/cars_shift_2025/presentation/navigation/Screen.kt
@@ -0,0 +1,18 @@
+package com.example.cars_shift_2025.presentation.navigation
+
+sealed class Screen (
+    val route: String
+){
+    object Main: Screen(ROUTE_MAIN)
+    object Error: Screen(ROUTE_ERROR)
+    object Loading: Screen(ROUTE_LOADING)
+    object CarWithRents : Screen("car_with_rents/{carId}") {
+        fun createRoute(carId: String) = "car_with_rents/$carId"
+    }
+
+    companion object {
+        const val ROUTE_MAIN = "main"
+        const val ROUTE_ERROR = "error"
+        const val ROUTE_LOADING = "loading"
+    }
+}
